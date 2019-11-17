c58584a483476a1fe6c3317562ad7679a2a2140b
Jan Ignatowicz <ignatowiczjan1@gmail.com>
2019-11-17 19:21:33 +0100

    BND-002 create database

---
+++ src/main/java/com/studio2/bgt/model/entity/Player.java
@@ -0,0 +1,70 @@
+package com.studio2.bgt.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "player_id")
+    private Long id;
+
+    @Column(nullable = false, name = "name")
+    private String name;
+
+    @Column(nullable = false, name = "email")
+    private String email;
+
+    @Column(nullable = false, name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "player", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private Set<Game> games;
+
+    @ManyToMany(cascade = {CascadeType.ALL})
+    @JoinTable(name = "friends",
+            joinColumns = {@JoinColumn(name = "player_id")},
+            inverseJoinColumns = {@JoinColumn(name = "friend_id")})
+    private Set<Player> friend1 = new HashSet<Player>();
+
+    @ManyToMany(mappedBy = "friend1")
+    private Set<Player> friend2 = new HashSet<Player>();
+
+    public Player(String name, String email, String password, Set<Game> games) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.games = games;
+        if (games != null) {
+            this.games.forEach(game -> game.setPlayer(this));
+        }
+    }
+
+    public Player(String name, String email, String password, Set<Game> games, Set<Player> friend1) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.games = games;
+        this.friend1 = friend1;
+        if (games != null) {
+            this.games.forEach(game -> game.setPlayer(this));
+        }
+    }
+
+    public void addFriend(Player player) {
+        this.friend1.add(player);
+        this.friend1.forEach(friend -> friend.getFriend2().add(this));
+    }
+}
