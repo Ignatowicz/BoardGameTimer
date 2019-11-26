5efe53046ebf72645f11a98550d9e2963cd9cd8b
Bartosz Gaj <bartosz.gaj4@gmail.com>
2019-11-27 00:16:59 +0100

    added recycler view

--- Frontend/app/src/main/java/com/example/boardgametimer/ui/main/MainActivity.java
+++ Frontend/app/src/main/java/com/example/boardgametimer/ui/main/MainActivity.java
@@ -4,20 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.boardgametimer.ui.newgame.NewGameActivity;
+import com.example.boardgametimer.ui.settings.SettingsActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.boardgametimer.R;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-
+public class MainActivity extends AppCompatActivity implements MyAdapter.ItemClickListener {
+    MyAdapter adapter;
 
 
     @Override
@@ -26,7 +31,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
 
-        final Button newGameButton = findViewById(R.id.new_game_button);
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
+
+        final Button settingsButton = findViewById(R.id.settings_button);
+        final Button friendsButton = findViewById(R.id.friends_button);
 
         setSupportActionBar(toolbar);
 
@@ -34,18 +55,30 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        newGameButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, NewGameActivity.class);
                 startActivity(intent);
             }
         });
-    }
 
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        friendsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+        @Override
+        public void onItemClick(View view, int position) {
+            Intent intent = new Intent(MainActivity.this, NewGameActivity.class);
+            startActivity(intent);
+        }
 }
