9b11a37e228b9aa237cff7b8aca1439927b6a4c2
Jan Ignatowicz <ignatowiczjan1@gmail.com>
2019-11-27 18:07:25 +0100

    BGT-004 restructure due to notification

--- Backend/src/main/java/com/studio2/bgt/controller/AbstractController.java
+++ Backend/src/main/java/com/studio2/bgt/controller/AbstractController.java
@@ -1,11 +1,11 @@
 package com.studio2.bgt.controller;
 
 import com.studio2.bgt.model.entity.Player;
+import com.studio2.bgt.model.enums.SendType;
 import com.studio2.bgt.model.helpers.PlayHelper;
+import com.studio2.bgt.model.helpers.StartGameNotificationHelper;
 
-import java.util.HashSet;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 public abstract class AbstractController {
 
@@ -50,6 +50,32 @@ public abstract class AbstractController {
         player.getFriend2().forEach(f -> f.setFriend1(new HashSet<>()));
         player.getFriend2().forEach(f -> f.setFriend2(new HashSet<>()));
         return player;
+    }
+
+    protected StartGameNotificationHelper preparePlayersToWhomNotificationWillBeSent(PlayHelper play, SendType sendType) {
+        StartGameNotificationHelper startGameNotification = new StartGameNotificationHelper();
+        Map<String, String> players = new HashMap<>();
+        Set<String> topics = new HashSet<>();
+
+        switch (sendType) {
+            case FRIENDS:
+                play.getFriends().forEach(f -> players.put(String.valueOf(f.getId()), f.getName()));
+                play.getFriends().forEach(f -> topics.add("Player_" + f.getId()));
+            case ACCEPTED:
+                play.getAccepted().forEach(f -> players.put(String.valueOf(f.getId()), f.getName()));
+                play.getAccepted().forEach(f -> topics.add("Player_" + f.getId()));
+            case PLAYERS_TOUR_A:
+                play.getPlayersTourA().forEach(f -> players.put(String.valueOf(f.getId()), f.getName()));
+                play.getPlayersTourA().forEach(f -> topics.add("Player_" + f.getId()));
+            case PLAYERS_TOUR_B:
+                play.getPlayersTourB().forEach(f -> players.put(String.valueOf(f.getId()), f.getName()));
+                play.getPlayersTourB().forEach(f -> topics.add("Player_" + f.getId()));
+        }
+
+        startGameNotification.setPlayers(players);
+        startGameNotification.setTopics(topics);
+
+        return startGameNotification;
     }
 
 }
---
+++ Backend/src/main/java/com/studio2/bgt/model/enums/SendType.java
@@ -0,0 +1,8 @@
+package com.studio2.bgt.model.enums;
+
+public enum SendType {
+    FRIENDS,
+    ACCEPTED,
+    PLAYERS_TOUR_A,
+    PLAYERS_TOUR_B
+}
